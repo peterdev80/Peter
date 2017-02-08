1382620d7298f0843bd6ed1c57c71f451b2b9e1f
gbdev <peter.dev@live.com>
2017-02-08 11:06:25 +0300

    08

--- AndroidProject/CriminalIntent/app/src/main/java/com/example/peter/criminalintent/CrimeFragment.java
+++ AndroidProject/CriminalIntent/app/src/main/java/com/example/peter/criminalintent/CrimeFragment.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 /**
@@ -16,6 +20,8 @@ import android.widget.EditText;
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +48,28 @@ public class CrimeFragment extends Fragment {
 // И здесь тоже
             }
         });
+        mDateButton = (Button)v.findViewById(R.id.crime_date);
+       String dt = DateFormat.format("EEEE, dd MMMM yyyy", mCrime.getDate()).toString();
+
+
+        mDateButton.setText(dt);
+        mDateButton.setEnabled(false);
+        mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean
+                    isChecked) {
+// Назначение флага раскрытия преступления
+                mCrime.setSolved(isChecked);
+            }
+        });
+
+
+
+
+
         return v;
+
+
     }
 }
